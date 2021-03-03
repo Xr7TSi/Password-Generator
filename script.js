613721219d32cf359f405cafc20fc00ee7b5a3b0
Jonathan Rein <jrein1296@gmail.com>
2021-03-02 22:16:57 -0500

    Function generatePassword is working.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
-
-
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -17,12 +15,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var numberOfCharacters = []
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = ["!", "#", "$", "%", "&", "*", "+", "-", "<", ">", "?", "@"];
 var characterBucket = [];
+var passwordToStringify = [];
 var password = [];
 
 
@@ -56,18 +55,8 @@ function generatePassword() {
   }  
 
   for (var i = 0; i < numberOfCharactersResponse; i++) {
-
-    function getPassword(items)
-  {
-    
-  return characterBucket[Math.floor(Math.random()*characterBucket.length)];
-      
+   passwordToStringify.push (characterBucket[Math.floor(Math.random()*characterBucket.length)]);
   }
-
-  console.log(getPassword(characterBucket));
-
+   password = (passwordToStringify.join(''));
   }
-
-  }
-
 
--- script.sandbox.js
+++ script.sandbox.js
@@ -1,59 +1,62 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialCharacters = ["!", "#", "$", "%", "&", "*", "+", "-", "<", ">", "?", "@"]
-var characterBucket = []
-var password = []
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function generateCharacterBucket() {
-
-  var upperCaseResponse= prompt ("Do you want to include upper case letters? Please respond Y or N.")
   
-  if (upperCaseResponse === "Y") {
-    console.log("Yes")} else {
-    console.log("No")
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["!", "#", "$", "%", "&", "*", "+", "-", "<", ">", "?", "@"];
+var characterBucket = [];
+var passwordToStringify = [];
+var password = [];
+
+
+function generatePassword() {
+
+  var numberOfCharactersResponse = prompt ("How long should your password be?");
+  var upperCaseResponse = prompt ("Do you want to include upper case letters? Please respond y or n.");
+  var lowerCaseResponse = prompt ("Do you want to include lower case letters? Please respond y or n.");
+  var numberResponse = prompt ("Do you want to include numbers? Please respond y or n.");
+  var specialCharacterResponse = prompt ("Do you want to include special characters? Please respond y or n.");
+
+  if  ((numberOfCharactersResponse < 8) || (numberOfCharactersResponse > 128)) {
+    alert("Number of characters must be between 8 and 128.");
+     generatePassword();
     }
+
+  if (upperCaseResponse === "y") {
+    characterBucket = upperCase.concat(characterBucket);
+    } 
+
+  if (lowerCaseResponse === "y") {
+    characterBucket = lowerCase.concat(characterBucket);
+    } 
+
+  if (numberResponse === "y") {
+    characterBucket = number.concat(characterBucket);
+    } 
+
+  if (specialCharacterResponse === "y") {
+    characterBucket = specialCharacter.concat(characterBucket);
+  }  
+
+  for (var i = 0; i < numberOfCharactersResponse; i++) {
+   passwordToStringify.push (characterBucket[Math.floor(Math.random()*characterBucket.length)]);
   }
-
-  var upperCaseResponse= prompt ("Do you want to include upper case letters? Please respond Y or N.");
-
-      if (upperCaseResponse === "Y") {
-          var characterBucket = upperCase;
-          console.log(characterBucket);
-          } 
+   password = (passwordToStringify.join(''));
+   
+  }
