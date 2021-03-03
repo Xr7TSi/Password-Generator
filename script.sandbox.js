163246fff4de2ec84f009f8e01beb347359f8963
Jonathan Rein <jrein1296@gmail.com>
2021-03-03 00:31:00 -0500

    Commiting fully funnctionl code!

--- script.sandbox.js
+++ script.sandbox.js
@@ -34,31 +34,57 @@ function generatePassword() {
   var specialCharacterResponse = prompt ("Do you want to include special characters? Please respond y or n.");
 
   if  ((numberOfCharactersResponse < 8) || (numberOfCharactersResponse > 128)) {
-    alert("Number of characters must be between 8 and 128."); numberOfCharactersResponse = (0);
+    alert("Number of characters must be between 8 and 128."); numberOfCharactersResponse = (null);
      generatePassword();
     }
 
   if (upperCaseResponse === "y") {
     characterBucket = upperCase.concat(characterBucket);
-    } 
+    } else if (upperCaseResponse === "n") {
+      characterBucket = characterBucket} 
+      else{ numberOfCharactersResponse = (null); upperCaseResponse = (null);
+      alert("Please respond with lower case y or n.");
+      generatePassword();
+      }
 
   if (lowerCaseResponse === "y") {
     characterBucket = lowerCase.concat(characterBucket);
-    } 
+    } else if (lowerCaseResponse === "n") {
+      characterBucket = characterBucket} 
+      else{ numberOfCharactersResponse = (null); upperCaseResponse = (null); lowerCaseResponse =(null);
+      alert("Please respond with lower case y or n.");
+      generatePassword();
+      }
 
   if (numberResponse === "y") {
     characterBucket = number.concat(characterBucket);
-    } 
+    } else if (numberResponse === "n") {
+      characterBucket = characterBucket} 
+      else{ numberOfCharactersResponse = (null); upperCaseResponse = (null); lowerCaseResponse =(null); numberResponse = (null);
+      alert("Please respond with lower case y or n.");
+      generatePassword();
+      }
 
   if (specialCharacterResponse === "y") {
     characterBucket = specialCharacter.concat(characterBucket);
-  }  
+  }  else if (specialCharacterResponse === "n") {
+    characterBucket = characterBucket} 
+    else{ numberOfCharactersResponse = (null); upperCaseResponse = (null); lowerCaseResponse =(null); numberResponse = (null); specialCharacterResponse = (null);
+    alert("Please respond with lower case y or n.");
+    generatePassword();
+    }
 
   for (var i = 0; i < numberOfCharactersResponse; i++) {
    passwordToStringify.push (characterBucket[Math.floor(Math.random()*characterBucket.length)]);
   }
   password = (passwordToStringify.join(''));
 
+  if (upperCaseResponse === "n" && lowerCaseResponse === "n" && numberResponse === "n" && specialCharacterResponse === "n") {
+    numberOfCharactersResponse = (null); upperCaseResponse = (null); lowerCaseResponse =(null); numberResponse = (null); specialCharacterResponse = (null);
+    alert("Please select at least one character type.");
+    generatePassword();
+  } 
+
   return password
-  
+
   }
